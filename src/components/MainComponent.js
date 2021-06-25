ae27f0034840a9960060c36f2847dc81007d0d47
Krosis855 <jtspalding22@gmail.com>
2021-06-24 20:59:43 -0700

    Adding Breadcrumbs

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -23,7 +23,6 @@ class Main extends Component {
     }
 
     render() {
-
         const HomePage = () => {
             return (
                 <Home
@@ -34,6 +33,16 @@ class Main extends Component {
             );
         };
 
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+
+                />
+            )
+        }
+
         return (
             <div>
                 <Header />
@@ -41,6 +50,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId}></Route>
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
